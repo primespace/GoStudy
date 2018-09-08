08e3524b6013cf81b740fb347aadcfffb9d4761f
typark <dogameadmin@gmail.com>
2018-09-08 13:18:26 +0900

    Add ForTest.go

---
+++ Array.go
@@ -0,0 +1,13 @@
+package main
+
+func main() {
+	var a [3]int
+	a[0] = 1
+	a[1] = 2
+	a[2] = 3
+	println(a[1])
+
+	var a1 = [3]int{1, 2, 3}
+	var a3 = [...]int{1, 2, 3}
+
+}
--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 
+go build ForTest.go
 
 go build AnonymousFunction.go
+go build Array.go
