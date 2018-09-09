97afe353984863f467ca8e16e8935db0b39c5d2c
typark <dogameadmin@gmail.com>
2018-09-10 01:12:59 +0900

    Rename files

--- src/main/build.sh
+++ src/main/build.sh
@@ -1,20 +1,4 @@
 
-go build ForTest.go
 
-go build AnonymousFunction.go
-go build Array.go
-go build Slice.go
-go build Map.go
-
-go build Package.go
-go build Struct.go
-go build Method.go
-go build Interface.go
-go build TypeAssert.go
-go build Errors.go
-go build Defer.go
-go build Panic.go
-go build Recover.go
-go build Goroutine.go
-go build Goroutine2.go
-go build Channels.go
+go build arrayTest.go
+go build forTest.go
