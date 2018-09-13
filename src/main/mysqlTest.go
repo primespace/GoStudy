a83542d9bd8f2c3c94c0b0d05f26bff1c765571f
typark <dogameadmin@gmail.com>
2018-09-13 23:40:49 +0900

    .

--- src/main/mysqlTest.go
+++ src/main/mysqlTest.go
@@ -10,17 +10,33 @@ import (
 
 func main() {
 
-	db, err := sql.Open("mysql", "")
+	db, err := sql.Open("mysql", "root:@tcp(localhost)/chartschool")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer db.Close()
 
-	var name string
-	err = db.QueryRow("select aaa from dual").Scan(&name)
+	rows, err := db.Query("select nickname from user")
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer rows.Close()
 
-	fmt.Println(name)
+	columns, err := rows.Columns()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("columns ", len(columns))
+
+	for rows.Next() {
+		rows.Scan
+	}
+
+	// var name string
+	// err = db.QueryRow("select nickname from user").Scan(&name)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// fmt.Println(name)
 }
